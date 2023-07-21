1b370dcd308d9383a9dafab22b616e05742b7382
janak <janakchy01@gamil.com>
2023-07-21 14:16:50 +0545

    Project created and Configured

---
+++ src/main/java/com/assignment/javamvc/JavaMvcAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.javamvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMvcAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMvcAssignmentApplication.class, args);
+	}
+
+}
